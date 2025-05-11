3d09f27bf1e15634eb3a70e47f89c890f209ed2d
SatoruuGoyoo <mentabullrichgregorio@gmail.com>
2025-05-11 13:56:18 -0300

    Making Menu
    Created main menu for the game, tested with profilier and correct minor leaks

---
+++ My project/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button _playGame;
+    [SerializeField] private Button _exitGame;
+
+    private void Awake()
+    {
+        _playGame.onClick.AddListener(PlayGame);
+        _exitGame.onClick.AddListener(ExitGame);
+    }
+
+    private void PlayGame()
+    {
+        // Load the game scene
+        SceneManager.LoadScene("TestRoomZ1");
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
