1e79a54d3a037ea135f2df10d725b5e5c93b1e58
Sebarbosa10 <sebastianbarbosa10@gmail.com>
2025-05-13 18:51:29 -0300

    correction of WinZone
    --new version without trigger

--- My project/Assets/Scripts/WinZone.cs
+++ My project/Assets/Scripts/WinZone.cs
@@ -2,14 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class WinZone : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] private Transform playerTransform;
+
+    private BoxCollider boxCollider;
+    private Collider[] buffer = new Collider[1];
+
+    void Awake()
     {
-        if (other.CompareTag("Player"))
+        boxCollider = GetComponent<BoxCollider>();
+        if (boxCollider == null)
         {
-            
-            Application.Quit();
+            Debug.LogError("WinZone necesita un BoxCollider.");
+            return;
+        }
+
+        var updateManager = FindFirstObjectByType<CustomUpdateManager>();
+        updateManager.Register(new WinZoneLogic(boxCollider, playerTransform, buffer));
+    }
+
+    private class WinZoneLogic : IUpdatable
+    {
+        private readonly BoxCollider boxCollider;
+        private readonly Transform playerTransform;
+        private readonly Collider[] buffer;
+
+        public WinZoneLogic(BoxCollider boxCollider, Transform playerTransform, Collider[] buffer)
+        {
+            this.boxCollider = boxCollider;
+            this.playerTransform = playerTransform;
+            this.buffer = buffer;
+        }
+
+        public void Tick(float deltaTime)
+        {
+            Vector3 center = boxCollider.bounds.center;
+            Vector3 halfExtents = boxCollider.bounds.extents;
+            Quaternion rotation = boxCollider.transform.rotation;
+
+            int hits = Physics.OverlapBoxNonAlloc(center, halfExtents, buffer, rotation);
+            for (int i = 0; i < hits; i++)
+            {
+                if (buffer[i] != null && buffer[i].transform == playerTransform)
+                {
+                    Debug.Log("¡Victoria! El jugador tocó la zona.");
+                    Application.Quit();
+                }
+            }
         }
     }
 }
