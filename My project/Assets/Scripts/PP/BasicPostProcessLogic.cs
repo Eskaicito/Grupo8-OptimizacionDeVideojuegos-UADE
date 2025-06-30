09508ced9af60e33c3ba5460044bf04581504cea
Nicolas Saldeno <skyladdamebusiness@gmail.com>
2025-06-30 13:54:11 -0300

    adding PP scripts

---
+++ My project/Assets/Scripts/PP/BasicPostProcessLogic.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BasicPostProcessLogic : IUpdatable
+{
+    private readonly Material mat;
+    private readonly Camera camera;
+
+    public BasicPostProcessLogic(Camera camera, Shader shader)
+    {
+        this.camera = camera;
+        mat = new Material(shader);
+    }
+
+    public void Tick(float deltaTime)
+    {
+        
+    }
+
+    public void RenderPostProcess(RenderTexture source, RenderTexture destination)
+    {
+        source.wrapMode = TextureWrapMode.Mirror;
+        Graphics.Blit(source, destination, mat);
+    }
+}
